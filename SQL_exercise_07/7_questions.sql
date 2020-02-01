8e5b7423ed2740239e546d3b5ae22781f2e4fb5c
jusliu <jus8447321@gmail.com>
2020-02-02 00:09:06 +0800

    2/1 SQL practice

--- SQL_exercise_07/7_questions.sql
+++ SQL_exercise_07/7_questions.sql
@@ -1,4 +1,14 @@
 -- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express 
 -- 7.1 Who receieved a 1.5kg package?
     -- The result is "Al Gore's Head".
+SELECT Client.Name
+FROM Package INNER JOIN Client ON Client.AccountNumber = Package.Recipient
+WHERE Package.Weight = 1.5;
 -- 7.2 What is the total weight of all the packages that he sent?
+SELECT SUM(Weight)
+FROM Package
+WHERE Sender = (
+	SELECT Client.AccountNumber
+	FROM Package INNER JOIN Client ON Client.AccountNumber = Package.Recipient
+	WHERE Package.Weight = 1.5
+);
