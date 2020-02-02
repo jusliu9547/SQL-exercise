024779b9034853b8971296f8f67c1a26d8144264
jusliu <jus8447321@gmail.com>
2020-02-02 13:59:47 +0800

    2/2 SQL practice

--- SQL_exercise_10/10_questions.sql
+++ SQL_exercise_10/10_questions.sql
@@ -1,4 +1,18 @@
 -- 10.1 Join table PEOPLE and ADDRESS, but keep only one address information for each person (we don't mind which record we take for each person). 
     -- i.e., the joined table should have the same number of rows as table PEOPLE
+SELECT *
+  FROM People
+    LEFT JOIN 
+    (SELECT *
+      FROM Address
+      GROUP BY id) AS Address
+    ON People.id = Address.id;
 -- 10.2 Join table PEOPLE and ADDRESS, but ONLY keep the LATEST address information for each person. 
     -- i.e., the joined table should have the same number of rows as table PEOPLE
+SELECT *
+FROM People
+LEFT JOIN
+(SELECT id, address, MAX(updatedate)
+  FROM Address
+  GROUP BY id) AS Address
+ ON People.id = Address.id;
